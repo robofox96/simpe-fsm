da98b1ce74113355f5fea13d90293adbb4b73365
61698 <cars24@LAP-013352.local>
2024-09-10 14:51:53 +0530

    Initial Commit

---
+++ src/main/java/com/example/cf_finite_state_machine/CFFiniteStateMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.cf_finite_state_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CFFiniteStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CFFiniteStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cf_finite_state_machine/CFFiniteStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cf_finite_state_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CFFiniteStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
